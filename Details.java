58603d76cb5414fdc880210abb5b5fcee2ede672
onyemachiprosper38-debug <onyemachiprosper38@gmail.com>
2026-05-17 19:26:26 +0100

    Add files via upload

---
+++ Details.java
@@ -0,0 +1,28 @@
+import java.util.Scanner
+  public class Details {
+      public static void main(String[] args) {
+      Scanner input = new Scanner (System.in);
+    
+      System.out.print("Enter FirstNme");
+      String firstname = input.next();
+      System.out.print("Enter MidleName"):
+      String midlename = input.next();
+      System.out.print("Enter LastName");
+      String lastname = input.next();
+      System.out.print("Enter Address");
+      String address = input.next();
+      System.out.print("Enter Sex");
+      String sex = input.next();
+      System.out.print("Enter Age");
+      int age = input.next();
+      System.out.print("Enter Email");
+      String email = input.next();
+      System.out.print("Enter phone number");
+      String phone number = input.next();
+
+
+
+  }
+
+
+}
